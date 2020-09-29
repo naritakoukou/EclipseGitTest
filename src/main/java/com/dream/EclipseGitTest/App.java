99da42665d8afce207c5ba612ccec40e7da2fbe4
eclipse_user <elipse_user@2020.com>
2020-09-30 07:35:47 +0900

    this is my first commit by eclipse.

---
+++ src/main/java/com/dream/EclipseGitTest/App.java
@@ -0,0 +1,13 @@
+package com.dream.EclipseGitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
